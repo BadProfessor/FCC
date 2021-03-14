d8755d4bdb5bd2e447849487f90a78a8b6928c08
Juraj Jarmek <badjjj@live.com>
2021-03-14 22:15:33 +0000

    quotes

--- 3. Front End Certification/7. Front End Certification Projects/markdown-previewer/src/App.js
+++ 3. Front End Certification/7. Front End Certification Projects/markdown-previewer/src/App.js
@@ -1,39 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// we need in codepen React, ReactDOM, Bootstrap
+import marked from 'marked';
+// we need in codepen React, ReactDOM, Bootstrap, marked library
+// npm install --save marked
+
+const initialState = `
+This is a paragraph
+
+**This is bolded text**
+
+> Block Quotes!
+
+# Heading
+
+## Heading
+
+- list item 1
+- list item 2
+- list item 3
+
+
+[Visit my website](http://example.com/)
+
+This is inline code: \`<div></div>\`
+
+This is a block of code:
+
+\`\`\`
+let x = 1;
+let y = 2;
+let z = x + y;
+\`\`\`
+
+![React](https://www.freecodecamp.org/icons/icon-96x96.png?v=6cba562cbd10e31af925a976f3db73f7)
+`;
 
 class App extends React.Component {
   state = {
-    markdown: '',
+    text: initialState,
   };
 
   handleChange = (e) => {
     this.setState({
-      markdown: e.target.value,
+      text: e.target.value,
     });
   };
 
   render() {
-    const { markdown } = this.state;
+    const { text } = this.state;
+
+    const markdown = marked(text, { breaks: true });
 
     return (
       <div>
-        <h2 className="text-center m-4">Convert your Markdown</h2>
+        <h2 className="text-center m-4 text-white">Convert your Markdown</h2>
         <div className="row">
           <div className="col-6">
-            <h5>Enter your Markdown here:</h5>
+            <h5 className="text-center text-white">Enter your markdown:</h5>
             <textarea
-              className="form-control"
+              className="form-control p-2"
               id="editor"
-              value={markdown}
+              value={text}
               onChange={this.handleChange}
             />
           </div>
-          <div className="col-6" id="preview">
-            <h5>See the result:</h5>
-            <div className="preview">
-              <p>Hello Preview</p>
-            </div>
+          <div className="col-6">
+            <h5 className="text-center text-white">See the result:</h5>
+            <div
+              className="preview rounded"
+              id="preview"
+              dangerouslySetInnerHTML={{ __html: markdown }}
+            />
           </div>
         </div>
       </div>
