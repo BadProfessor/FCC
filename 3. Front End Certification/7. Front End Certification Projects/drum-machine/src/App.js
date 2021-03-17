27bf7bb80c04c01f4b69a305e66c75672c1f34ce
Juraj Jarmek <badjjj@live.com>
2021-03-17 19:45:27 +0000

    drum

--- 3. Front End Certification/7. Front End Certification Projects/drum-machine/src/App.js
+++ 3. Front End Certification/7. Front End Certification Projects/drum-machine/src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import React, ReactDOM
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <p>Hello</p>
     </div>
   );
-}
+};
+
+const Box = (props) => {
+  <div className="box">{props.text}</div>;
+};
 
 // uncomment this in codepen.js
 // ReactDOM.render(<App />, document.getElementById('root'));
 
+// delete the following in codepen
 export default App;
 
 //https://www.youtube.com/watch?v=gXUshKPc-_g
-// 3:20
+// 7:05
 // pen: https://codepen.io/badprofessor/pen/NWbmmwJ
