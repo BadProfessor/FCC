8b65ad02b7bb1d8cb3438b362dd9983cef685fcc
Juraj Jarmek <badjjj@live.com>
2021-03-24 20:14:05 +0000

    calculator

---
+++ 3. Front End Certification/7. Front End Certification Projects/calculator/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// delete above in codepen
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Hello</p>
+      </header>
+    </div>
+  );
+}
+
+// uncomment this in codepen
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// delete this in codepen
+export default App;
+
+// https://www.youtube.com/watch?v=NGOzAaJRPQU
+// 0:00
