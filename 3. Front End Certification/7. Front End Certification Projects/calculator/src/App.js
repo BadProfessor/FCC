a8c6c32baf6e62f6d71580154b358152bd410f44
Juraj Jarmek <badjjj@live.com>
2021-03-30 23:04:03 +0100

    calc

--- 3. Front End Certification/7. Front End Certification Projects/calculator/src/App.js
+++ 3. Front End Certification/7. Front End Certification Projects/calculator/src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const ops = ['/', '*', '-', '+', '='];
+const ops = ['/', '*', '-', '+'];
+const ids = {
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  0: 'zero',
+  '/': 'divide',
+  '*': 'multiply',
+  '-': 'subtract',
+  '+': 'add',
+};
 
 class App extends React.Component {
   state = {
@@ -13,8 +29,6 @@ class App extends React.Component {
   handleClick = (e) => {
     const { calc, lastPressed } = this.state;
     const { innerText } = e.target;
-
-    console.log(innerText);
 
     switch (innerText) {
       case 'AC': {
@@ -30,12 +44,29 @@ class App extends React.Component {
         });
         break;
       }
+      case '.': {
+        const splitted = calc.split(/[\+\-\*\/]/);
+        const last = splitted.slice(-1)[0];
+
+        if (!last.includes('.')) {
+          this.setState({
+            calc: calc + '.',
+          });
+        }
+
+        break;
+      }
+
       default: {
         let e = undefined;
 
         if (ops.includes(innerText)) {
-          if (ops.includes(lastPressed)) {
-            e = calc.slice(0, -3) + ` ${innerText} `;
+          if (ops.includes(lastPressed) && innerText !== '-') {
+            const lastNumberIdx = calc
+              .split('')
+              .reverse()
+              .findIndex((char) => char !== ' ' && nums.includes(+char));
+            e = calc.slice(0, calc.length - lastNumberIdx) + ` ${innerText} `;
           } else {
             e = `${calc} ${innerText} `;
           }
@@ -45,10 +76,13 @@ class App extends React.Component {
 
         this.setState({
           calc: e,
-          lastPressed: innerText,
         });
       }
     }
+
+    this.setState({
+      lastPressed: innerText,
+    });
   };
 
   render() {
@@ -63,28 +97,45 @@ class App extends React.Component {
           {calc}
         </div>
         <div className="nums-container">
-          <button className="big-h light-grey ac" onClick={this.handleClick}>
+          <button
+            className="big-h light-grey ac"
+            id="clear"
+            onClick={this.handleClick}
+          >
             AC
           </button>
           {nums.map((num) => (
             <button
               className={`dark-grey ${num === 0 && 'big-h'}`}
+              id={ids[num]}
               key={num}
               onClick={this.handleClick}
             >
               {num}
             </button>
           ))}
-          <button className="light-grey" onClick={this.handleClick}>
+          <button
+            className="light-grey"
+            id="decimal"
+            onClick={this.handleClick}
+          >
             .
           </button>
         </div>
         <div className="ops-container">
           {ops.map((op) => (
-            <button className="orange" key={op} onClick={this.handleClick}>
+            <button
+              className="orange"
+              id={ids[op]}
+              key={op}
+              onClick={this.handleClick}
+            >
               {op}
             </button>
           ))}
+          <button className="orange" id="equals" onClick={this.handleClick}>
+            =
+          </button>
         </div>
       </div>
     );
