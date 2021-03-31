dd801289824d8f185908703aaeca9da877ffc493
Juraj Jarmek <badjjj@live.com>
2021-03-31 23:04:59 +0100

    test

--- 3. Front End Certification/7. Front End Certification Projects/pomodoro/src/App.js
+++ 3. Front End Certification/7. Front End Certification Projects/pomodoro/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+// copy from here
 
-function App() {
-  return (
-    <div className="App">
-      <p>test</p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SetTimer />
+      </div>
+    );
+  }
 }
+
+const SetTimer = () => (
+  <div className="timer-container">
+    <h1>Break Time</h1>
+    <div>
+      <button>
+        <i className="fas fa-minus" />
+      </button>
+      <span>5</span>
+      <button>
+        <i className="fas fa-plus" />
+      </button>
+    </div>
+  </div>
+);
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -15,7 +33,8 @@ function App() {
 //   document.getElementById('root')
 // );
 
+// copy till here
 export default App;
 
 // https://www.youtube.com/watch?v=5rz6XbrCqt0
-// 1:00
+// 12:00
