eb3584cac8f42fc5fa3ae461a3da6bcb87b89ffd
Juraj Jarmek <badjjj@live.com>
2021-05-02 13:19:21 +0100

    store

--- 3. Front End Development Libraries/8. Videos/2. React Tutorial - Build an e-commerce site from scratch using React and Netlify/store/src/App.js
+++ 3. Front End Development Libraries/8. Videos/2. React Tutorial - Build an e-commerce site from scratch using React and Netlify/store/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// to import the bootstrap dependency, had to run:
+// yarn add babel-loader @babel/core --dev
+// and then
+// yarn remove babel-loader @babel/core --dev
+// and for some weird reason it worked.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">Column Number 1</div>
+        <div className="col-6">
+          <span>
+            <i className="fas fa-home"></i>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
