1e7c93920347ed7621e292afbc4a1260f652601a
Juraj Jarmek <badjjj@live.com>
2021-05-03 20:05:49 +0100

    test

--- 3. Front End Development Libraries/8. Videos/2. React Tutorial - Build an e-commerce site from scratch using React and Netlify/store/src/App.js
+++ 3. Front End Development Libraries/8. Videos/2. React Tutorial - Build an e-commerce site from scratch using React and Netlify/store/src/App.js
@@ -1,25 +1,35 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+      <Details />
+      <Cart />
+      <Default />
+    </React.Fragment>
+  );
+}
+
+export default App;
+
 // to import the bootstrap dependency, had to run:
 // yarn add babel-loader @babel/core --dev
 // and then
 // yarn remove babel-loader @babel/core --dev
 // and for some weird reason it worked.
-
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">Column Number 1</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home"></i>
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
