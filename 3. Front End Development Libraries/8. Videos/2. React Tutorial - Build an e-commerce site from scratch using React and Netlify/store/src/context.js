1c59b8f7a62eb0d22eefdaefbafe3e4cac88aaa0
Juraj Jarmek <badjjj@live.com>
2021-05-24 21:34:16 +0100

    shop

--- 3. Front End Development Libraries/8. Videos/2. React Tutorial - Build an e-commerce site from scratch using React and Netlify/store/src/context.js
+++ 3. Front End Development Libraries/8. Videos/2. React Tutorial - Build an e-commerce site from scratch using React and Netlify/store/src/context.js
@@ -3,13 +3,13 @@ import { storeProducts, detailProduct } from './data';
 
 const ProductContext = React.createContext();
 
-//Provider
-//Consumer
-
 class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
+    cart: [],
+    modalOpen: true, //return it to false after testing
+    modalProduct: detailProduct,
   };
 
   componentDidMount() {
@@ -27,12 +27,50 @@ class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log('hello from detail');
+  getItem = (id) => {
+    const product = this.state.products.find((item) => item.id === id);
+    return product;
+  };
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
 
   addToCart = (id) => {
-    console.log(`hello from add to cart.id is ${id}`);
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(
+      () => {
+        return {
+          products: tempProducts,
+          cart: [...this.state.cart],
+        };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
+
+  closeModal = () => {
+    this.setState(() => {
+      return { modalProduct: false };
+    });
   };
 
   render() {
@@ -42,6 +80,8 @@ class ProductProvider extends Component {
           ...this.state,
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
+          openModal: this.openModal,
+          closeModal: this.closeModal,
         }}
       >
         {this.props.children}
