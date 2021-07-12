c90ad2170b10e04de9e6541f4f741d433b9a04f9
Juraj Jarmek <badjjj@live.com>
2021-07-12 16:20:25 +0100

    test

--- 4. Data Visualization/3. Data Visualization Projects/2. Visualize Data with a Scatterplot Graph/scatterplot-graph/src/index.js
+++ 4. Data Visualization/3. Data Visualization Projects/2. Visualize Data with a Scatterplot Graph/scatterplot-graph/src/index.js
@@ -7,12 +7,22 @@ fetch(
 )
   .then((res) => res.json())
   .then((res) => {
-    //
-    createStuff(res.map((r) => [convertMinAndSec(r.Time), new Date(r.Year)]));
+    createStuff(
+      res.map((r) => [convertMinAndSec(r.Time), r.Year, r.Doping, r.Name])
+    );
   });
 
 function convertMinAndSec(str) {
   return new Date(`2010 01 01 00:${str}`);
+}
+
+function createInnerHTMLForTooltip(d) {
+  return `
+    <p>${d[3]}(${d[1]})</p>
+    <p>Time: <strong>${d[0].getMinutes()}:${d[0].getSeconds()}</strong></p>
+
+    ${d[2] ? `<small>${d[2]}</small>` : ''}
+  `;
 }
 
 function createStuff(data) {
@@ -20,7 +30,7 @@ function createStuff(data) {
   const height = 400;
   const padding = 40;
 
-  console.log(data);
+  const circleRadius = 7;
 
   const yScale = d3
     .scaleTime()
@@ -29,8 +39,13 @@ function createStuff(data) {
 
   const xScale = d3
     .scaleTime()
-    .domain([d3.min(data, (d) => d[1]), d3.max(data, (d) => d[1])])
+    .domain([
+      d3.min(data, (d) => new Date(d[1] - 1)),
+      d3.max(data, (d) => new Date(d[1] + 1)),
+    ])
     .range([padding, width - padding]);
+
+  console.log(d3.max(data, (d) => d[1]));
 
   const svg = d3
     .select('#container')
@@ -38,9 +53,39 @@ function createStuff(data) {
     .attr('width', width)
     .attr('height', height);
 
+  // create the graph
+  svg
+    .selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('data-xvalue', (d) => d[1])
+    .attr('data-yvalue', (d) => d[0])
+    .attr('cx', (d) => xScale(d[1]))
+    .attr('cy', (d) => yScale(d[0]))
+    .attr('fill', (d) => (d[2] === '' ? 'orange' : 'skyblue'))
+    .attr('stroke', 'black')
+    .attr('r', circleRadius)
+    .on('mouseover', (d, i) => {
+      tooltip.classList.add('show');
+      tooltip.style.left = xScale(d[1]) + 10 + 'px';
+      tooltip.style.top = yScale(d[0]) - 10 + 'px';
+      tooltip.setAttribute('data-year', d[1]);
+
+      tooltip.innerHTML = createInnerHTMLForTooltip(d);
+    })
+    .on('mouseout', () => {
+      tooltip.classList.remove('show');
+    });
+
+  // format the data
+  const timeFormatForMinAndSec = d3.timeFormat('%M:%S');
+  const timeFormatForYear = d3.format('d');
+
   // create axis
-  const xAxis = d3.axisBottom(xScale);
-  const yAxis = d3.axisLeft(yScale);
+  const xAxis = d3.axisBottom(xScale).tickFormat(timeFormatForYear);
+  const yAxis = d3.axisLeft(yScale).tickFormat(timeFormatForMinAndSec);
 
   svg
     .append('g')
@@ -54,16 +99,3 @@ function createStuff(data) {
     .attr('transform', `translate(${padding}, 0)`)
     .call(yAxis);
 }
-
-// // Needed for React
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <div>
-//       <p>Hello from Index.js</p>
-//     </div>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
