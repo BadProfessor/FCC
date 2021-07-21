3f911019f9492165583c4c7f512129a7179681bd
Juraj Jarmek <badjjj@live.com>
2021-07-21 13:01:12 +0100

    test

--- 0. Videos/5. AWS Basics for Beginners/Code and Files/DynamoDB/DynamoDB CLI Commands.sh
+++ 0. Videos/5. AWS Basics for Beginners/Code and Files/DynamoDB/DynamoDB CLI Commands.sh
@@ -9,7 +9,7 @@ aws dynamodb scan --table-name mystore
 #### QUERIES ####
 
 # Use Key-Conditions Parameter:
-aws dynamodb query  --table-name mystore --key-conditions '{ "clientid":{ "ComparisonOperator":"EQ", "AttributeValueList": [ {"S": "chris@example.com"} ] } }'
+aws dynamodb query --table-name mystore --key-conditions '{ \"clientid\":{ \"ComparisonOperator\":\"EQ\", \"AttributeValueList\": [ {\"S\": \"chris@example.com\"} ] } }'
 
 # Use Key-Condition-Expression Parameter:
-aws dynamodb query --table-name mystore --key-condition-expression "clientid = :name" --expression-attribute-values '{":name":{"S":"chris@example.com"}}'
+aws dynamodb query --table-name mystore --key-condition-expression \"clientid = :name\" --expression-attribute-values '{\":name\":{\"S\":\"chris@example.com\"}}'
